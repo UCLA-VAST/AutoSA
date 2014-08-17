0a7546c96f98a185c27e743fc15d66f61c43143d
Sven Verdoolaege <skimo@kotnet.org>
2014-08-17 12:33:34 +0200

    gpu.c: move parametrization to schedule.c
    We will be extracting the array reference grouping into a separate file
    and both the moved code and the code that remains in gpu.c use this function.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- schedule.c
+++ schedule.c
@@ -53,6 +53,67 @@ __isl_give isl_map *projection(__isl_take isl_space *dim,
     return project_out(dim, src_len, dst_len, src_len - dst_len);
 }
 
+/* Add parameters with identifiers "ids" to "set".
+ */
+static __isl_give isl_set *add_params(__isl_take isl_set *set,
+	__isl_keep isl_id_list *ids)
+{
+	int i, n;
+	unsigned nparam;
+
+	n = isl_id_list_n_id(ids);
+
+	nparam = isl_set_dim(set, isl_dim_param);
+	set = isl_set_add_dims(set, isl_dim_param, n);
+
+	for (i = 0; i < n; ++i) {
+		isl_id *id;
+
+		id = isl_id_list_get_id(ids, i);
+		set = isl_set_set_dim_id(set, isl_dim_param, nparam + i, id);
+	}
+
+	return set;
+}
+
+/* Equate the dimensions of "set" starting at "first" to
+ * freshly created parameters with identifiers "ids".
+ * The number of equated dimensions is equal to the number of elements in "ids".
+ */
+static __isl_give isl_set *parametrize(__isl_take isl_set *set,
+	int first, __isl_keep isl_id_list *ids)
+{
+	int i, n;
+	unsigned nparam;
+
+	nparam = isl_set_dim(set, isl_dim_param);
+
+	set = add_params(set, ids);
+
+	n = isl_id_list_n_id(ids);
+	for (i = 0; i < n; ++i)
+		set = isl_set_equate(set, isl_dim_param, nparam + i,
+					isl_dim_set, first + i);
+
+	return set;
+}
+
+/* Given a parameter space "space", create a set of dimension "len"
+ * of which the dimensions starting at "first" are equated to
+ * freshly created parameters with identifiers "ids".
+ */
+__isl_give isl_set *parametrization(__isl_take isl_space *space,
+	int len, int first, __isl_keep isl_id_list *ids)
+{
+	isl_set *set;
+
+	space = isl_space_set_from_params(space);
+	space = isl_space_add_dims(space, isl_dim_set, len);
+	set = isl_set_universe(space);
+
+	return parametrize(set, first, ids);
+}
+
 /* Extend "set" with unconstrained coordinates to a total length of "dst_len".
  */
 __isl_give isl_set *extend(__isl_take isl_set *set, int dst_len)
--- schedule.h
+++ schedule.h
@@ -51,6 +51,8 @@ __isl_give isl_map *project_out(__isl_take isl_space *dim,
 	int len, int first, int n);
 __isl_give isl_map *projection(__isl_take isl_space *dim,
 	int src_len, int dst_len);
+__isl_give isl_set *parametrization(__isl_take isl_space *space,
+	int len, int first, __isl_keep isl_id_list *names);
 __isl_give isl_set *extend(__isl_take isl_set *set, int dst_len);
 __isl_give isl_union_map *align_range(__isl_take isl_union_map *umap);
 
