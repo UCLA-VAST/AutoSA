6bbda4c2e13fc2e381df887a9a6ea18a668946b7
Sven Verdoolaege <skimo@kotnet.org>
2015-06-22 16:45:16 +0200

    move gpu_stmt_access and gpu_stmt to gpu.h
    It is not clear why they were ever put in schedule.h.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- gpu.h
+++ gpu.h
@@ -2,10 +2,49 @@
 #define _GPU_H
 
 #include <isl/ast.h>
+#include <isl/id.h>
 #include <isl/id_to_ast_expr.h>
+
+#include <pet.h>
 
 #include "ppcg.h"
 #include "ppcg_options.h"
+
+/* An access to an outer array element or an iterator.
+ * Accesses to iterators have an access relation that maps to an unnamed space.
+ * An access may be both read and write.
+ * If the access relation is empty, then the output dimension may
+ * not be equal to the dimension of the corresponding array.
+ */
+struct gpu_stmt_access {
+	/* Access reads elements */
+	int read;
+	/* Access writes elements */
+	int write;
+	/* All writes are definite writes. */
+	int exact_write;
+	/* The number of index expressions specified in the access. */
+	int n_index;
+
+	/* May access relation */
+	isl_map *access;
+	/* May access relation with as domain a mapping from iteration domain
+	 * to a reference identifier.
+	 */
+	isl_map *tagged_access;
+	/* The reference id of the corresponding pet_expr. */
+	isl_id *ref_id;
+
+	struct gpu_stmt_access *next;
+};
+
+struct gpu_stmt {
+	isl_id *id;
+	struct pet_stmt *stmt;
+
+	/* Linked list of accesses. */
+	struct gpu_stmt_access *accesses;
+};
 
 /* Represents an outer array possibly accessed by a gpu_prog.
  */
