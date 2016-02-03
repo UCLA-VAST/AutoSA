983e44e9ce79e650f0227adf6fceac98064fca23
Sven Verdoolaege <skimo@kotnet.org>
2016-02-03 17:39:47 +0100

    drop ppcg_print_guarded
    Its final use was removed in the previous commit.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- print.h
+++ print.h
@@ -23,9 +23,4 @@ __isl_give isl_printer *ppcg_print_exposed_declarations(
 __isl_give isl_printer *ppcg_print_hidden_declarations(
 	__isl_take isl_printer *p, struct ppcg_scop *scop);
 
-__isl_give isl_printer *ppcg_print_guarded(__isl_take isl_printer *p,
-	__isl_take isl_set *guard, __isl_take isl_set *context,
-	__isl_give isl_printer *(*fn)(__isl_take isl_printer *p, void *user),
-	void *user);
-
 #endif
