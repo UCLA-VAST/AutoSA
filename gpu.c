41dc345a912561e6ba71c9a969a44c3345897831
Sven Verdoolaege <skimo@kotnet.org>
2016-02-10 15:58:46 +0100

    gpu.c: map_to_device: drop test for context node
    It is not clear why the test was added in c237c5a (gpu: insert context node
    in final schedule tree, Wed Oct 2 11:06:07 2013 +0200).
    The context node is unconditionally added inside map_to_device,
    so the tested note is guaranteed to be a context node.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- gpu.c
+++ gpu.c
@@ -4991,8 +4991,7 @@ static __isl_give isl_schedule *map_to_device(struct gpu_gen *gen,
 	node = isl_schedule_get_root(schedule);
 	isl_schedule_free(schedule);
 	node = isl_schedule_node_child(node, 0);
-	if (isl_schedule_node_get_type(node) == isl_schedule_node_context)
-		node = isl_schedule_node_child(node, 0);
+	node = isl_schedule_node_child(node, 0);
 	node = isolate_permutable_subtrees(node, gen->prog);
 	domain = isl_schedule_node_get_domain(node);
 	prefix = isl_schedule_node_get_prefix_schedule_union_map(node);
